86b313647c2a20fcd1bef337e96fe2a4c558139b
You <michael@theablecoder.io>
2022-11-14 20:41:18 +1100

    broken but not for long

---
+++ FastReadFileBase.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Application
+{
+    public static class FastReadFileBase
+    {
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2004:RemoveCallsToGCKeepAlive")]
+        public static T[] FastRead<T>(FileStream fs, int count) where T : struct
+        {
+            int sizeOfT = Marshal.SizeOf(typeof(T));
+
+            long bytesRemaining = fs.Length - fs.Position;
+            long wantedBytes = count * sizeOfT;
+            long bytesAvailable = Math.Min(bytesRemaining, wantedBytes);
+            long availableValues = bytesAvailable / sizeOfT;
+            long bytesToRead = (availableValues * sizeOfT);
+
+            if ((bytesRemaining < wantedBytes) && ((bytesRemaining - bytesToRead) > 0))
+                Console.WriteLine("Requested data exceeds available data and partial data remains in the file.");
+
+            T[] result = new T[availableValues];
+
+            GCHandle gcHandle = GCHandle.Alloc(result, GCHandleType.Pinned);
+
+            var ipp = new System.Threading.NativeOverlapped();  // need this with above pInvoke
+
+            try
+            {
+                uint bytesRead;
+                if (!ReadFile(
+                    fs.SafeFileHandle,
+                    gcHandle.AddrOfPinnedObject(),
+                    (uint)bytesToRead,
+                    out bytesRead, ref ipp))
+                {
+                    throw new IOException("Unable to read file.", new Win32Exception(Marshal.GetLastWin32Error()));
+                }
+                Debug.Assert(bytesRead == bytesToRead);
+            }
+
+            finally
+            {
+                gcHandle.Free();
+            }
+
+            GC.KeepAlive(fs);
+
+            return result;
+        }
+        // https://stackoverflow.com/questions/66789631/fastest-way-to-read-large-binary-file-into-array-of-int-in-c-sharp/67332253#67332253
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadFile(Microsoft.Win32.SafeHandles.SafeFileHandle hFile, [Out] IntPtr lpBuffer, uint nNumberOfBytesToRead,
+                                out uint lpNumberOfBytesRead, [In] ref System.Threading.NativeOverlapped lpOverlapped);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,66 @@
 ﻿
 
 using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Application;
+using Windows.Storage;
 
 namespace Application
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-
-            var fileName = args[0];
-            long offset = 0x10000000; // 256 megabytes
-            long length = 0x20000000; // 512 megabytes
-            using (var mmf = MemoryMappedFile.CreateFromFile(fileName, FileMode.Open, "filename"))
+            var folderPath = args[0];
+            // enumerate through the files in the folder recursively
+            var files = Directory.EnumerateFiles(folderPath, "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
             {
-                // Create a random access view, from the 256th megabyte (the offset)
-                // to the 768th megabyte (the offset plus length).
-                using (var accessor = mmf.CreateViewAccessor(offset, length))
+                byte[] b = FastReadFileBase.FastRead<byte>(new FileStream(file, FileMode.Open), int.MaxValue);
+                string mixedString = Encoding.ASCII.GetString(b);
+                // Use regex to filter out anynon ascii characters
+                var sortedString = Regex.Replace(mixedString, @"[^\x20-\x7E]", string.Empty);
+                // remove any new line or return characters and split on spaces
+                var words = sortedString.Split(' ');
+                foreach (var word in words)
                 {
-                    int bufferSize = 1024;
+                    var questionSortedString = Regex.Replace(word, @"(\?{2,})", string.Empty);
+                    // regex search for any email addresses according to rfc 5322, or ip address according to rfc 3986, or url according to rfc 3986
+                    var email = Regex.Match(questionSortedString, @"^(([^<>()[\]\\.,;:\s@\""]+"
+                        + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+                        + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|"
+                        + @"(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", RegexOptions.IgnoreCase);
+                    var ip = Regex.Match(questionSortedString, @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", RegexOptions.IgnoreCase);
+                    var url = Regex.Match(questionSortedString, @"^((http|https|ftp)://)?(www.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$", RegexOptions.IgnoreCase);
+                    var outputfile = @"C:\Users\micha\Documents\output.txt";
+                    var emailvalue = email.Success ? email.Value : "";
+                    var ipvalue = ip.Success ? ip.Value : "";
+                    var urlvalue = url.Success ? url.Value : "";
+                    await WriteFileAsync(Outputfile, emailvalue);
+                    await WriteFileAsync(outputfile, ipvalue);
+                    await WriteFileAsync(outputfile, urlvalue);
 
 
-                    // Make changes to the view.
-                    for (long i = 0; i < length; i += bufferSize)
-                    {
-                      accessor.Read(i, out int buffer);
-                        global::System.Console.WriteLine(buffer.ToString());
-                    }
                 }
             }
         }
+
+        static async Task WriteFileAsync(Windows.Storage.IStorageFile file, string[] content)
+        {
+            Console.WriteLine("Async Write File has started.");
+            if (file != null)
+            {
+                await FileIO.AppendLinesAsync(file, content);
+            }
+            Console.WriteLine("Async Write File has completed.");
+        }
     }
 }
+
 
